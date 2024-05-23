47c8fb3362d2df083f4fdb267b5c399f69b12bbc
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-22 20:26:44 -0400

    Update database.tf

--- database.tf
+++ database.tf
@@ -1,1 +1,17 @@
+provider "azurerm" {
+  features {}
+}
 
+variable "config_file" {
+  description = "Path to the YAML configuration file"
+  default     = "config.yaml"
+}
+
+locals {
+  config = yamldecode(file(var.config_file))
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = local.config.resource_group_name
+  location = local.config.location
+}
